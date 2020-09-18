c03c4d9dc2d7bc1da3759209f7094a7464ed4526
loma <57.loma@gmail.com>
2020-09-18 17:06:46 +0700

    share admin/user build with nginx volumn

--- docker/script/admin.sh
+++ docker/script/admin.sh
@@ -3,4 +3,4 @@
 npm install
 sed -i "s/52.53.243.250:8000/localhost/g" src/environments/environment.ts
 #./node_modules/@angular/cli/bin/ng serve --baseHref=/admin/ --host=0.0.0.0 --port=4200 --disableHostCheck=true --liveReload=false --optimization=true --watch=false
-./node_modules/@angular/cli/bin/ng build --baseHref=/web-driver-admin/ --optimization=true
+./node_modules/@angular/cli/bin/ng build --baseHref=/web-driver-admin/ --optimization=true 
--- docker/script/user.sh
+++ docker/script/user.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 npm install
-sed -i "s/54.193.25.161:8000/localhost/g" src/app/config.js
+sed -i "s/52.53.243.250:8000/localhost/g" src/app/config.js
+sed -i "s/52.53.243.250:8000/localhost/g" src/environments/environment.ts
 #./node_modules/@angular/cli/bin/ng serve --baseHref=/user/ --host=0.0.0.0 --port=4300 --disableHostCheck=true --liveReload=false --optimization=true --watch=false --verbose=true --progress=true
-node --max_old_space_size=8192 ./node_modules/@angular/cli/bin/ng build --baseHref=/wb-driver/ --optimization=true
+node --max_old_space_size=8192 ./node_modules/@angular/cli/bin/ng build --baseHref=/wb-driver/
 
