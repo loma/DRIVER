12b510cbaf56eb296ee919f1a6ecca53d85c7d0d
loma <57.loma@gmail.com>
2020-10-03 15:55:27 +0700

    update fixed api config

--- docker/script/user.sh
+++ docker/script/user.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 npm install
-sed -i "s/52.53.243.250:8000/localhost:8080/g" src/app/config.js
-sed -i "s/52.53.243.250:8000/localhost:8080/g" src/environments/environment.ts
 sed -i "s/54.193.25.161:8000/localhost:8080/g" src/app/config.js
 sed -i "s/54.193.25.161:8000/localhost:8080/g" src/environments/environment.ts
+
+sed -i "s/54.215.143.19:8000/localhost:8080/g" src/app/config.js
+sed -i "s/54.215.143.19:8000/localhost:8080/g" src/environments/environment.ts
+
 node --max_old_space_size=8192 ./node_modules/@angular/cli/bin/ng build --baseHref=/WB-Driver/ --source-map=false
 
